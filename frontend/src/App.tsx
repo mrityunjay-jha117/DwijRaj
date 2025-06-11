8a5aa7fa715dcedf190fd79e2d28c5a780bbba79
mrityunjay jha <158163203+mrityunjay-jha117@users.noreply.github.com>
2025-06-11 10:35:06 +0530

    vercel spa fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,13 @@ import BlogPage from "./pages/blogpage";
 import { ScrollToTop } from "./useful_functions/scrolltotop";
 import { Toaster } from "react-hot-toast";
 export default function App() {
- return (
+  return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
       <BrowserRouter>
         <ScrollToTop />
         <Routes>
-          <Route path="/home" element={<PoetryLayout />} />
+          <Route path="/" element={<PoetryLayout />} />
           <Route path="/blog_page" element={<BlogPage />} />
         </Routes>
       </BrowserRouter>
